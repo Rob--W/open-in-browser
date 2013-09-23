122b3867ca8493932bbb351c6ecd4e15c781f852
Rob Wu <gwnRob@gmail.com>
2013-09-23 16:46:37 +0200

    redirectUrl shim for onHeadersReceived
    The method added in this commit is not used yet, but provides code
    that provides redirectUrl functionality for stages that do not yet
    support onHeadersReceived.
    ( feature request: http://crbug.com/280464 )
    
    See also https://github.com/Rob--W/chrome-api/tree/master/chrome.tabs.executeScriptInFrame

---
+++ extension/open-with.js
@@ -0,0 +1,51 @@
+/**
+ * (c) 2013 Rob Wu <gwnRob@gmail.com>
+ */
+(function() {
+'use strict';
+window.openWith = openWith;
+
+/**
+ * Open With
+ * @param config {object} Object that specifies how the URL ought to be opened.
+ * @param details {object} Object from chrome.webRequest.onHeadersReceived
+ */
+function openWith(config, details) {
+    // Original URL
+    var url = details.url;
+    // TODO: Actually use config to redirect elsewhere.
+    var targetUrl = url + '?';
+
+    navigateToUrl(details.tabId, details.frameId, targetUrl);
+}
+
+/**
+ * Load a new URL in a given frame/tab.
+ */
+function navigateToUrl(tabId, frameId, url) {
+    if (frameId === 0) { // Main frame
+        chrome.tabs.update(tabId, {
+            url: url
+        });
+        return;
+    }
+    // Use meta-refresh redirection to blank the frame's content before navigating
+    // to a different page. Otherwise, the user might see "This page is blocked by ...",
+    // which is confusing (use case of this method: Abort a request and redirect to new URL).
+    // Note: This whole method (navigateToUrl) will be obsolete once redirectUrl is implemented
+    // for onHeadersReceived - see https://code.google.com/p/chromium/issues/detail?id=280464
+    var code = 'location.href = \'data:text/html,<meta http-equiv="refresh" content="0;' +
+             url.replace(/"/g, '&quot;') + '">\';';
+    // https://github.com/Rob--W/chrome-api/tree/master/chrome.tabs.executeScriptInFrame
+    chrome.tabs.executeScriptInTab(tabId, {
+        frameId: frameId,
+        code: code
+    }, function(result) {
+        if (!result) { // Did the tab disappear? Is the frame inaccessible?
+            chrome.tabs.create({
+                url: url
+            });
+        }
+    });
+}
+})();
