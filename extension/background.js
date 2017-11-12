3e54914d46f95d3936abd03aa59834f76caa85a2
Rob Wu <rob@robwu.nl>
2017-11-12 21:17:07 +0100

    List supported MIME types of Firefox instead of Chrome
    Fixes #1

--- extension/background.js
+++ extension/background.js
@@ -23,7 +23,13 @@ chrome.webRequest.onHeadersReceived.addListener(async function(details) {
 
     if (!contentDisposition || !r_contentDispositionAttachment.test(contentDisposition)) {
         // Content disposition != attachment. Let's take a look at the MIME-type.
-        if (!shouldInterceptRequest(mimeType)) {
+        let canDisplayInline = ContentHandlers.canDisplayInline(originalCT);
+        if (typeof canDisplayInline !== 'boolean') {
+            abortionObserver.setupBeforeAsyncTask(null);
+            canDisplayInline = await canDisplayInline;
+            if (!abortionObserver.continueAfterAsyncTask()) return;
+        }
+        if (canDisplayInline) {
             if (Prefs.get('text-nosniff')) {
                 let unsniffableContentType = ContentHandlers.makeUnsniffableContentType(originalCT);
                 if (unsniffableContentType !== originalCT.contentType) {
@@ -138,117 +144,6 @@ function setHeader(headers, headerName, headerValue) {
         name: headerName,
         value: headerValue
     });
-}
-
-
-// The next MIME types are always rendered inline.
-var MIME_TYPES_BUILTIN = [
-// Chrome extension
-// TODO: Consider removing the CRX MIME-typ and integrate https://github.com/Rob--W/crxviewer
-    'application/x-chrome-extension',
-// mime_util.cc  supported_image_types
-    'image/jpeg',
-    'image/pjpeg',
-    'image/jpg',
-    'image/webp',
-    'image/png',
-    'image/gif',
-    'image/bmp',
-    'image/vnd.microsoft.icon',
-    'image/x-icon',
-    'image/x-xbitmap',
-// mime_util.cc  common_media_types
-    'audio/ogg',
-    'application/ogg',
-    'video/ogg',
-    'video/webm',
-    'audio/webm',
-    'audio/wav',
-    'audio/x-wav',
-    'video/mp4',
-    'video/x-m4v',
-    'audio/mp4',
-    'audio/x-m4a',
-    'audio/mp3',
-    'audio/x-mp3',
-    'audio/mpeg',
-// mime_util.cc  supported_non_image_types
-    'text/cache-manifest',
-    'text/html',
-    'text/xml',
-    'text/xsl',
-    'text/plain',
-    'text/css',
-    'text/vnd.chromium.ftp-dir',
-//  'text/', // special case; handled below (TODO)
-    'image/svg+xml',
-    'application/xml',
-    'application/atom+xml',
-    'application/rss+xml',
-    'application/xhtml+xml',
-    'application/json',
-    'multipart/related',
-    'multipart/x-mixed-replace',
-// mime_util.cc  supported_certificate_types
-    'application/x-x509-user-cert',
-// mime_util.cc  supported_javascript_types
-    'text/javascript',
-    'text/ecmascript',
-    'application/javascript',
-    'application/ecmascript',
-    'application/x-javascript',
-    'text/javascript1.1',
-    'text/javascript1.2',
-    'text/javascript1.3',
-    'text/jscript',
-    'text/livescript'
-];
-
-// Chrome renders text/* as text (inline), except in the following cases:
-var MIME_TYPES_TEXT_SPECIAL_CASE = [
-// mime_util.cc  unsupported_text_types
-    'text/calendar',
-    'text/x-calendar',
-    'text/x-vcalendar',
-    'text/vcalendar',
-    'text/vcard',
-    'text/x-vcard',
-    'text/directory',
-    'text/ldif',
-    'text/qif',
-    'text/x-qif',
-    'text/x-csv',
-    'text/x-vcf',
-    'text/rtf',
-    'text/comma-separated-values',
-    'text/csv',
-    'text/tab-separated-values',
-    'text/tsv',
-    'text/ofx',
-    'text/vnd.sun.j2me.app-descriptor'
-];
-
-/**
- * Determines whether or not to intercept the request
- *
- * @param {string} mimeType The value of the Content-Type header
- * @return {boolean} Whether to intercept the request and show the prompt.
- */
-function shouldInterceptRequest(mimeType) {
-    if (!mimeType) {
-        // Mime-type not specified. For now, do nothing.
-        return false;
-    }
-    if (mimeType.slice(0, 5) === 'text/') {
-        // return true for Ignore text/*, unless Chromium's source code states that
-        // the particular text/... MIME-type should not be rendered as text.
-        return MIME_TYPES_TEXT_SPECIAL_CASE.indexOf(mimeType) !== -1;
-    }
-    if (MIME_TYPES_BUILTIN.indexOf(mimeType) !== -1) {
-        // Chrome will certainly inline these MIME-types.
-        return false;
-    }
-    return true;
 }
 
 /**
--- extension/content-handlers-firefox.js
+++ extension/content-handlers-firefox.js
@@ -45,3 +45,170 @@ ContentHandlers.makeUnsniffableContentType = function(contentType) {
     // Otherwise, don't change the Content-Type value.
     return contentType;
 };
+
+/** @type {null|Promise<boolean>|boolean} */
+var _pdfjsEnabled = null;
+
+/**
+ * @param {ParsedContentType} parsedCT
+ * @returns {boolean|Promise<boolean>} Whether the given type can be displayed inline.
+ *   The return value is usually a boolean, but sometimes a Promise<boolean> if the feature
+ *   detection method is aynchronous.
+ */
+ContentHandlers.canDisplayInline = function(parsedCT) {
+    let {mimeType} = parsedCT;
+    if (MIME_TYPES_HTML.includes(mimeType) ||
+        MIME_TYPES_TEXT.includes(mimeType) ||
+        MIME_TYPES_XML.includes(mimeType) ||
+        MIME_TYPES_IMAGES.includes(mimeType) ||
+        MIME_TYPES_OTHER.includes(mimeType)) {
+        return true;
+    }
+
+    if (mimeType === 'application/pdf') {
+        if (_pdfjsEnabled === null) {
+            _pdfjsEnabled = _checkPDFJSEnabled();
+            _pdfjsEnabled.then(pdfjsEnabled => {
+                _pdfjsEnabled = pdfjsEnabled;
+            });
+        }
+        return _pdfjsEnabled;
+    }
+
+    // Media types
+    if (mimeType.startsWith('audio/') ||
+        mimeType.startsWith('video/') ||
+        // https://searchfox.org/mozilla-central/rev/02bc7e2b1bcbab2d7b604ca713b42bcefd47ad2d/dom/media/DecoderTraits.cpp#52-54
+        mimeType === 'application/vnd.apple.mpegurl' ||
+        mimeType === 'application/x-mpegurl' ||
+        // https://searchfox.org/mozilla-central/rev/02bc7e2b1bcbab2d7b604ca713b42bcefd47ad2d/dom/media/ogg/OggDecoder.cpp#24
+        mimeType === 'application/ogg' ||
+        // https://searchfox.org/mozilla-central/rev/02bc7e2b1bcbab2d7b604ca713b42bcefd47ad2d/dom/media/flac/FlacDecoder.cpp#30
+        mimeType === 'application/x-flac') {
+        return new Audio().canPlayType(mimeType);
+    }
+    if (navigator.mimeTypes[mimeType]) {
+        // This is a Flash file, and Flash is enabled.
+        return true;
+    }
+
+    return false;
+};
+
+/**
+ * @returns {Promise<boolean>} Whether the browser can display PDF files inline.
+ */
+function _checkPDFJSEnabled() {
+    const PDF_DATA = `%PDF
+1 0 obj<</Type/Pages/Count 1/Kids[2 0 R]/MediaBox[0 0 1 1]>>endobj
+2 0 obj<</Type/Page/Parent 1 0 R>>endobj
+3 0 obj<</Type/Catalog/Pages 1 0 R>>endobj
+xref
+0 4
+0000000000 65535 f 
+0000000005 00000 n 
+0000000072 00000 n 
+0000000113 00000 n 
+trailer
+<</Root 3 0 R/Size 4>>
+startxref
+156
+%%EOF`;
+    return new Promise(resolve => {
+        var o = document.createElement('object');
+        o.type = 'application/pdf';
+        o.src = URL.createObjectURL(new Blob([PDF_DATA], {type: 'application/pdf'}));
+        o.onload = o.error = function({type}) {
+            o.onload = o.onerror = null;
+            URL.revokeObjectURL(o.src);
+            // Abort load to prevent PDF.js from continuing to parse and load the content.
+            o.data = '';
+            o.remove();
+            resolve(type === 'load');
+        };
+        setTimeout(function() {
+            // If somehow the PDF.js detection method fails to complete within reasonable time,
+            // assume that PDFs can be viewed inline (because this is the case by default).
+            if (o.onload) {
+                o.onload({type: 'load'});
+            }
+        }, 1000);
+        document.body.appendChild(o);
+    });
+}
+
+// Most of the following types are registered in Firefox to the "Gecko-Content-Viewers" category.
+// Some of the types (in MIME_TYPES_OTHER) are stream converters ("@mozilla.org/streamconv;1"),
+// content handler ("@mozilla.org/uriloader/content-handler;1") or
+// content listener (NS_CONTENT_LISTENER_CATEGORYMANAGER_ENTRY).
+const MIME_TYPES_HTML = [
+    // https://searchfox.org/mozilla-central/rev/7fb4cc447c06f14fe3b5c6b0c9d103a860937250/layout/build/nsContentDLF.cpp#53-57
+    'text/html',
+    'application/x-view-source',
+    'application/xhtml+xml',
+    'application/vnd.wap.xhtml+xml',
+];
+const MIME_TYPES_TEXT = [
+    // https://searchfox.org/mozilla-central/rev/7fb4cc447c06f14fe3b5c6b0c9d103a860937250/dom/base/nsContentUtils.cpp#4306-4330
+    'application/javascript',
+    'application/x-javascript',
+    'text/ecmascript',
+    'application/ecmascript',
+    'text/javascript',
+    'application/json',
+    'text/json',
+    'text/plain',
+    'text/css',
+    'text/cache-manifest',
+    'text/vtt',
+];
+const MIME_TYPES_XML = [
+    // https://searchfox.org/mozilla-central/rev/7fb4cc447c06f14fe3b5c6b0c9d103a860937250/layout/build/nsContentDLF.cpp#61-66
+    'text/xml',
+    'application/xml',
+    'application/mathml+xml',
+    'application/rdf+xml',
+    'text/rdf',
+];
+const MIME_TYPES_IMAGES = [
+    // https://searchfox.org/mozilla-central/rev/7fb4cc447c06f14fe3b5c6b0c9d103a860937250/layout/build/nsContentDLF.cpp#71
+    'image/svg+xml',
+    // https://searchfox.org/mozilla-central/rev/7fb4cc447c06f14fe3b5c6b0c9d103a860937250/image/DecoderFactory.cpp#27-66
+    'image/png',
+    'image/x-png',
+    'image/apng',
+    'image/gif',
+    'image/jpeg',
+    'image/pjpeg',
+    'image/jpg',
+    'image/bmp',
+    'image/x-ms-bmp',
+    'image/x-icon',
+    'image/vnd.microsoft.icon',
+    'image/icon',
+];
+const MIME_TYPES_OTHER = [
+    // https://searchfox.org/mozilla-central/rev/7fb4cc447c06f14fe3b5c6b0c9d103a860937250/layout/build/nsContentDLF.cpp#76-77
+    'application/vnd.mozilla.xul+xml',
+    'mozilla.application/cached-xul',
+    // https://searchfox.org/mozilla-central/rev/7fb4cc447c06f14fe3b5c6b0c9d103a860937250/browser/components/feeds/BrowserFeeds.manifest#12-14
+    'application/vnd.mozilla.maybe.feed',
+    'application/vnd.mozilla.maybe.video.feed',
+    'application/vnd.mozilla.maybe.audio.feed',
+    // https://searchfox.org/mozilla-central/rev/7fb4cc447c06f14fe3b5c6b0c9d103a860937250/netwerk/build/nsNetModule.cpp#435-450
+    'application/http-index-format',
+    'multipart/x-mixed-replace',
+    'multipart/mixed',
+    'multipart/byteranges',
+    // Note: The following type triggers MIME sniffing, and might or might not be displayed inline.
+    'application/x-unknown-content-type',
+    'application/mac-binhex40',
+    // https://searchfox.org/mozilla-central/rev/7fb4cc447c06f14fe3b5c6b0c9d103a860937250/toolkit/mozapps/extensions/extensions.manifest#14
+    'application/x-xpinstall',
+    // https://searchfox.org/mozilla-central/rev/7fb4cc447c06f14fe3b5c6b0c9d103a860937250/security/manager/ssl/PSMContentListener.cpp#59-68
+    // https://searchfox.org/mozilla-central/rev/7fb4cc447c06f14fe3b5c6b0c9d103a860937250/security/manager/ssl/nsNSSModule.cpp#257-260
+    'application/x-x509-ca-cert',
+    'application/x-x509-server-cert',
+    'application/x-x509-user-cert',
+    'application/x-x509-email-cert',
+];
